0b4f0a26e15acc169fbd163d7fafcbc436391cf0
rulkimi <azrulkimi00@gmail.com>
2023-12-31 00:59:10 +0800

    get flags from RestCountries API

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const weather = document.querySelector('.js-weather');
 
 searchBtn.addEventListener('click', () => {
   const locationName = location.value;
-  location.value = '';
 
   const url = `http://localhost:8000/search-weather?place=${encodeURIComponent(locationName)}`;
 
@@ -22,6 +21,31 @@ searchBtn.addEventListener('click', () => {
       place.innerText = data.place;
       weather.innerText = data.temperature + data.unit;
       description.innerText = data.description;
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error('Fetch error:', error);
+    });
+});
+
+const flagImage = document.getElementById('countryFlag');
+const searchFlagBtn = document.querySelector('.js-search-flag-btn');
+const countryInput = document.querySelector('.js-country');
+
+searchFlagBtn.addEventListener('click', () => {
+  const countryName = countryInput.value;
+  
+  const url = `http://localhost:8000/search-flags?country_name=${encodeURIComponent(countryName)}`;
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Handle the response data
+      flagImage.src = data;
     })
     .catch((error) => {
       // Handle errors
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from requests_html import HTMLSession
+import requests
 
 app = FastAPI()
 session = HTMLSession()
@@ -14,9 +15,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def create_request_session(url):
+    return session.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'})
+
 def scrape_weather_data(place):
-    url = f'https://www.google.com/search?q=weather+{place}'
-    request = session.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'})
+    request = create_request_session(f'https://www.google.com/search?q=weather+{place}')
     
     # Check if the request was successful
     if request.status_code != 200:
@@ -30,8 +33,27 @@ def scrape_weather_data(place):
         return {"place": place, "temperature": temperature, "unit": unit, "description": description}
     except AttributeError:
         raise HTTPException(status_code=500, detail="Failed to retrieve weather data.")
+    
+def get_country_flag(country_name):
+    # Using the Restcountries API to get country data
+    api_url = f'https://restcountries.com/v3.1/name/{country_name}?fields=flags'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data and 'flags' in data[0]:
+            return data[0]['flags'].get('png', None)
+        else:
+            return None
+    else:
+        return None
 
 @app.get("/search-weather")
 def search_weather(place: str):
     weather_data = scrape_weather_data(place)
     return weather_data
+
+@app.get("/search-flags")
+def search_flags(country_name: str):
+    flag = get_country_flag(country_name)
+    return flag
