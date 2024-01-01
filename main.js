86489f5ddcea41914cc516d155fe3f3167beb988
rulkimi <azrulkimi00@gmail.com>
2024-01-01 19:19:40 +0800

    ui fixes

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const location = document.querySelector('.js-location');
 const searchBtn = document.querySelector('.js-search-btn');
 const place = document.querySelector('.js-place');
-const description = document.querySelector('.js-description');
 const weather = document.querySelector('.js-weather');
+const description = document.querySelector('.js-description');
+const precipitation = document.querySelector('.js-precipitation');
+const humidity = document.querySelector('.js-humidity');
+const wind = document.querySelector('.js-wind');
+
 
 searchBtn.addEventListener('click', () => {
   const locationName = location.value;
@@ -21,6 +25,9 @@ searchBtn.addEventListener('click', () => {
       place.innerText = data.place;
       weather.innerText = data.temperature + data.unit;
       description.innerText = data.description;
+      precipitation.innerText = data.precipitation;
+      humidity.innerText = data.humidity;
+      wind.innerText = data.wind;
     })
     .catch((error) => {
       // Handle errors
