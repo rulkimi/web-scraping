7419391062d51924c4a8c4ac7e29b13d9da094df
rulkimi <azrulkimi00@gmail.com>
2024-01-01 18:54:49 +0800

    remove get flags api

--- main.js
+++ main.js
@@ -27,28 +27,3 @@ searchBtn.addEventListener('click', () => {
       console.error('Fetch error:', error);
     });
 });
-
-const flagImage = document.getElementById('countryFlag');
-const searchFlagBtn = document.querySelector('.js-search-flag-btn');
-const countryInput = document.querySelector('.js-country');
-
-searchFlagBtn.addEventListener('click', () => {
-  const countryName = countryInput.value;
-  
-  const url = `http://localhost:8000/search-flags?country_name=${encodeURIComponent(countryName)}`;
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Handle the response data
-      flagImage.src = data;
-    })
-    .catch((error) => {
-      // Handle errors
-      console.error('Fetch error:', error);
-    });
-});
